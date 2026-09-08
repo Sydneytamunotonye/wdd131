5a1732ca9871bc77944d6e85ac838afceef8246f
Sydneytamunotonye <Sydneytamunotonye1@gmail.com>
2026-09-08 16:08:01 +0100

    week01-mod3

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,3 @@
-// Dynamically output the current year into the footer's first paragraph
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
-
-// Dynamically output the date the document was last modified
 document.getElementById("lastmodified").textContent = document.lastModified;
